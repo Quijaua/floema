3634b9d9429bb9c763eccfd5523489ddb64832be
Cauã <cauaserpa092@gmail.com>
2023-09-28 15:08:48 -0300

    Atualizando Mais opções de gestão de cores. #39

--- sql/doacoes.sql
+++ sql/doacoes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 28-Ago-2023 às 06:26
+-- Tempo de geração: 28-Set-2023 às 20:05
 -- Versão do servidor: 10.4.22-MariaDB
 -- versão do PHP: 8.1.1
 
@@ -38,9 +38,9 @@ CREATE TABLE `tb_checkout` (
   `facebook` varchar(255) DEFAULT NULL,
   `instagram` varchar(255) DEFAULT NULL,
   `linkedin` varchar(255) DEFAULT NULL,
+  `twitter` varchar(255) DEFAULT NULL,
   `youtube` varchar(255) DEFAULT NULL,
   `website` varchar(255) DEFAULT NULL,
-  `twitter` varchar(255) DEFAULT NULL,
   `cep` varchar(255) NOT NULL,
   `rua` varchar(255) NOT NULL,
   `numero` varchar(255) DEFAULT NULL,
@@ -49,8 +49,12 @@ CREATE TABLE `tb_checkout` (
   `estado` varchar(255) NOT NULL,
   `telefone` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
+  `nav_background` varchar(255) NOT NULL,
+  `nav_color` varchar(255) NOT NULL,
+  `background` varchar(255) NOT NULL,
   `color` varchar(255) NOT NULL,
   `hover` varchar(255) NOT NULL,
+  `text_color` varchar(255) NOT NULL,
   `load_btn` varchar(255) NOT NULL,
   `progress` varchar(255) NOT NULL,
   `monthly_1` varchar(255) NOT NULL,
@@ -74,8 +78,8 @@ CREATE TABLE `tb_checkout` (
 -- Extraindo dados da tabela `tb_checkout`
 --
 
-INSERT INTO `tb_checkout` (`id`, `nome`, `logo`, `title`, `descricao`, `privacidade`, `faq`, `facebook`, `instagram`, `linkedin`, `youtube`, `twitter`, `website`, `cep`, `rua`, `numero`, `bairro`, `cidade`, `estado`, `telefone`, `email`, `color`, `hover`, `load_btn`, `progress`, `monthly_1`, `monthly_2`, `monthly_3`, `monthly_4`, `monthly_5`, `yearly_1`, `yearly_2`, `yearly_3`, `yearly_4`, `yearly_5`, `once_1`, `once_2`, `once_3`, `once_4`, `once_5`) VALUES
-(1, 'Floema', 'floema-logo.png', '', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tempus tortor nec gravida pretium. Vestibulum ipsum diam, lacinia a est sit amet, tempor elementum odio. Phasellus vel eros sit amet dolor mollis ultricies id eu lectus. Nunc mattis magna id augue malesuada luctus. Donec sit amet diam id diam interdum sollicitudin.', 'https://seusite.com.br/politica-de-privacidade/', 'https://seusite.com.br/perguntas-frequentes/', 'https://facebook.com/seufacebook', 'https://facebook.com/seuinstagram', NULL, NULL, NULL, NULL, '11111-222', 'Rua Exemplo Nome da Rua', '999', 'Centro', 'São Paulo', 'SP', '(11) 9999-9999', 'suainstitucao@email.org.br', '#ffc107', '#212529', '#212529', '0, 204, 255', '20', '50', '100', '200', '300', '120', '240', '500', '1000', '2000', '100', '200', '500', '1000', '2000');
+INSERT INTO `tb_checkout` (`id`, `nome`, `logo`, `title`, `descricao`, `privacidade`, `faq`, `facebook`, `instagram`, `linkedin`, `twitter`, `youtube`, `website`, `cep`, `rua`, `numero`, `bairro`, `cidade`, `estado`, `telefone`, `email`, `nav_background`, `nav_color`, `background`, `color`, `hover`, `text_color`, `load_btn`, `progress`, `monthly_1`, `monthly_2`, `monthly_3`, `monthly_4`, `monthly_5`, `yearly_1`, `yearly_2`, `yearly_3`, `yearly_4`, `yearly_5`, `once_1`, `once_2`, `once_3`, `once_4`, `once_5`) VALUES
+(1, 'Floema', 'floema-logo.png', '', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tempus tortor nec gravida pretium. Vestibulum ipsum diam, lacinia a est sit amet, tempor elementum odio. Phasellus vel eros sit amet dolor mollis ultricies id eu lectus. Nunc mattis magna id augue malesuada luctus. Donec sit amet diam id diam interdum sollicitudin.', 'https://seusite.com.br/politica-de-privacidade/', 'https://seusite.com.br/perguntas-frequentes/', 'https://facebook.com/seufacebook', 'https://facebook.com/seuinstagram', NULL, 'https://twitter.com/seutwitter', NULL, NULL, '11111-222', 'Rua Exemplo Nome da Rua', '999', 'Centro', 'São Paulo', 'SP', '(11) 9999-9999', 'suainstitucao@email.org.br', '#ffffff', '#000000', '#ffffff', '#ffc107', '#212529', '#000000', '#212529', '0, 204, 255', '20', '50', '100', '200', '300', '120', '240', '500', '1000', '2000', '100', '200', '500', '1000', '2000');
 
 -- --------------------------------------------------------
 
@@ -104,23 +108,6 @@ CREATE TABLE `tb_clientes` (
   `newsletter` tinyint(1) NOT NULL,
   `private` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-INSERT INTO `tb_clientes` (`id`, `roles`, `nome`, `email`, `password`, `magic_link`, `phone`, `cpf`, `cep`, `endereco`, `numero`, `complemento`, `municipio`, `cidade`, `uf`, `asaas_id`, `newsletter`, `private`) VALUES (247, 1, 'Admin', 'admin@admin.com', '$2y$10$gphtP5ZDgkZNctcEhLKfs.MQ8qWc6Ebf8V6sqRf4q7QhClHSojT7.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0);
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `tb_integracoes`
---
-
-CREATE TABLE `tb_integracoes` (
-  `id` int(11) NOT NULL,
-  `fb_pixel` longtext DEFAULT NULL,
-  `gtm` longtext DEFAULT NULL,
-  `g_analytics` longtext DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-INSERT INTO `tb_integracoes` (`id`, `fb_pixel`, `gtm`, `g_analytics`) VALUES (1, '', '', '');
 
 -- --------------------------------------------------------
 
@@ -168,6 +155,26 @@ CREATE TABLE `tb_imagens` (
 
 INSERT INTO `tb_imagens` (`id`, `imagem`) VALUES
 (11, '1691640386.png');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_integracoes`
+--
+
+CREATE TABLE `tb_integracoes` (
+  `id` int(11) NOT NULL,
+  `fb_pixel` longtext DEFAULT NULL,
+  `gtm` longtext DEFAULT NULL,
+  `g_analytics` longtext DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `tb_integracoes`
+--
+
+INSERT INTO `tb_integracoes` (`id`, `fb_pixel`, `gtm`, `g_analytics`) VALUES
+(1, '', '', '');
 
 --
 -- Índices para tabelas despejadas
