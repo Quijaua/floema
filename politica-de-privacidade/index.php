1a634fcb4d1d77221ffc91a628887f2f3becd2ec
Rafael Bantu <34097797+rafaelbantu@users.noreply.github.com>
2023-10-22 20:38:31 -0300

    ajustes no medida do logotipo e descricao em politica de privacidade

--- politica-de-privacidade/index.php
+++ politica-de-privacidade/index.php
@@ -148,7 +148,7 @@
 <meta property="og:locale" content="pt_BR" />
 <meta property="og:type" content="website" />
 <meta property="og:title" content="<?php echo $title; ?>"/>
-<meta property="og:description" name="description" content="Lorem ipsum" />
+<meta property="og:description" name="description" content="<?php echo mb_strimwidth($descricao, 3, 120, '...'); ?>" />
 <meta property="og:url" value="<?php echo INCLUDE_PATH; ?>"/>
 <meta property="og:site_name" content="<?php echo $nome; ?>" />
 <meta property="article:modified_time" content="2022-12-01T18:38:06+00:00" />
@@ -160,7 +160,7 @@
 <meta name="twitter:url" value="<?php echo INCLUDE_PATH; ?>"/>
 <meta name="twitter:image" value="<?php echo INCLUDE_PATH; ?>assets/img/<?php echo $logo; ?>"/>
 <meta name="twitter:image" content="<?php echo INCLUDE_PATH; ?>assets/img/<?php echo $logo; ?>"/>
-<meta name="twitter:description" value="Lorem ipsum"/>
+<meta name="twitter:description" value="<?php echo mb_strimwidth($descricao, 3, 120, '...'); ?>"/>
 
 
 <script type="application/ld+json">{
@@ -176,7 +176,7 @@
 			},
 			"datePublished": "2023-03-02T19:50:30+00:00",
 			"dateModified": "2023-03-21T12:51:52+00:00",
-			"description": "Lorem ipsum",
+			"description": "<?php echo mb_strimwidth($descricao, 3, 120, '...'); ?>",
 			"inLanguage": "pt-BR",
 			"interactAction": [
 				{
@@ -224,7 +224,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-4 p-3">
-				<img src="../assets/img/<?php echo $logo; ?>" class="w-75">
+				<img src="../assets/img/<?php echo $logo; ?>">
 			</div>
 			<div class="col-md-8 mt-4">
 				<h1 class="h2"><?php echo ($title !== '') ? $title : 'Colabore com o Projeto '.$nome; ?></h1>
