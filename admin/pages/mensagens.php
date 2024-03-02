7bac77b5583f594f466522cb5ea8edf6d2adc99e
Rafael Bantu <34097797+rafaelbantu@users.noreply.github.com>
2024-03-02 15:55:28 -0300

    padronizando versão de bootstrap

--- admin/pages/mensagens.php
+++ admin/pages/mensagens.php
@@ -96,7 +96,7 @@
     </div>
 
 </div>
-<script src="https://cdn.ckeditor.com/ckeditor5/39.0.2/classic/ckeditor.js"></script>
+
 <script>
     ClassicEditor
     .create( document.querySelector( '#welcome_email' ), {})
--- admin/pages/politica-de-privacidade.php
+++ admin/pages/politica-de-privacidade.php
@@ -84,7 +84,6 @@
 
     </div>
 </div>
-<script src="https://cdn.ckeditor.com/ckeditor5/39.0.2/classic/ckeditor.js"></script>
 <script>
     ClassicEditor
     .create( document.querySelector( '#privacy_policy' ), {})
